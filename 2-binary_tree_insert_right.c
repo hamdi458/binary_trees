1162ad58aca5e9f427fe27a55a2f078f8e863a78
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-07-22 18:17:52 +0000

    tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,8 +10,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (!parent)
+		return (NULL);
 	node = malloc(sizeof(binary_tree_t));
-	if (!node || !parent)
+	if (!node)
 		return (NULL);
 	node->parent = parent;
 	node->n = value;
