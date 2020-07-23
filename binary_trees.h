c26fd93e38eaa5e35f06f8a92b2e089fae6f12e6
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-07-23 21:09:24 +0000

    tree

--- binary_trees.h
+++ binary_trees.h
@@ -19,6 +19,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 } binary_tree_t;
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
 size_t binary_tree_depth(const binary_tree_t *tree);
